a27c69f66540a670d691a0a7cacf10f2af711a95
nicole03 <ntidwell03@gmail.com>
2018-07-23 20:00:50 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,11 +1,11 @@
 puts "What is the guest's name?"
 guest_name = gets.chomp
-party_name = "50th Anniversary"
-date = "April 4th, 2045"
-time = "7 pm"
-host_name = "Val"
+party_name = gets.chomp
+date = gets.chomp
+time = gets.chomp
+host_name = gets.chomp
 
 puts"Dear #{guest_name}" 
-puts "You have been invited to my #{party_name}" 
-puts "It will take place at my house #{date} at #{time}" 
-puts "With all the love, #{host_name}" 
+puts "what type of party is my party?" #{party_name}" 
+puts "when and where will it take place #{date} at #{time}" 
+puts "What is my name? #{host_name}" 
