703bb085cd380e4e520616b1cdf87fbc1cc40d9d
nicole03 <ntidwell03@gmail.com>
2018-07-23 19:52:19 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -4,4 +4,7 @@ date = "April 4th, 2045"
 time = "7 pm"
 host_name = "Val"
 
-Dear #{guest_name}
+"Dear #{guest_name}" 
+"You have been invited to my #{party_name} 
+"It will take place at my house #{date} at #{time} 
+With all the love, #{host_name} 
